b95134786a9fb0886434ea4f1e41a8e7a3388db9
trjaffery <trjaffery04@gmail.com>
2024-12-16 18:27:22 -0600

    first commit

---
+++ src/main/java/com/khateeb/KhateebGPT/KhateebGptApplication.java
@@ -0,0 +1,13 @@
+package com.khateeb.KhateebGPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhateebGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhateebGptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khateeb/KhateebGPT/KhateebGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khateeb.KhateebGPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhateebGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
