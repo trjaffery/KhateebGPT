5f3b1ed92f38695d38ad0ffc031469e4a537a45c
Yusuf <yshaikh113@outlook.com>
2024-12-17 16:33:10 -0600

    file restructure

---
+++ src/main/java/com/khateeb/home/HomeController.java
@@ -0,0 +1,13 @@
+package com.khateeb.home;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot is working!";
+    }
+}
